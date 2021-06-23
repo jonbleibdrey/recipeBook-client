e1bfab9b565fa6cbf25681217172cf4516691965
jonathan bleibdrey <jonbleibdrey@aol.com>
2021-06-23 18:14:30 -0400

    added axios, got datat, mapped it and succesfully outputted it

--- src/components/Fetch.js
+++ src/components/Fetch.js
@@ -1,22 +1,34 @@
-import axios from 'axios'
-import React, {useEffect, useState} from 'react'
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Fetch = () => {
-    const [info, setInfo] = useState(null)
+  const [users, setUsers] = useState(null);
 
-    useEffect(() => {
-       axios.get("https://rails-jonathanb-heroku-test.herokuapp.com/")
-       .then((response) => {
-       setInfo(response.data)
+  useEffect(() => {
+    axios
+      .get("https://rails-jonathanb-heroku-test.herokuapp.com/")
+      .then((response) => {
+        setUsers(response.data);
+        console.log(users);
       });
-    }, [])
+    //eslint-disable-line react-hooks/exhaustive-deps
+  }, []);
 
-    return (
-        <div>
-            {info && info.map(e => console.log(e))}
-        </div>
-    )
-}
+  return (
+    <div>
+      {users &&
+        users.map((user) => (
+          <ul key={user.id}>
+            <li>
+              {" "}
+              <h1>{user.name}</h1>
+              <h3>{user.email}</h3>
+              <h4>Bio: {user.bio}</h4>
+            </li>
+          </ul>
+        ))}
+    </div>
+  );
+};
 
-export default Fetch
+export default Fetch;
